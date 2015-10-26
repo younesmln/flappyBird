9d6ea6f864278020450d7f907695a0f2fe2644f4
younes <dev.meliani@gmail.com>
2015-10-26 01:20:13 +0100

    intial commit

---
+++ Assets/scripts/Bird.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+	float speed = 15;
+	float force = 600;
+
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * this.speed;
+	}
+
+	void LateUpdate () {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * this.force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/scripts/GroundFellow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFellow : MonoBehaviour {
+	
+	public Transform target;
+	
+	void Update () {
+		transform.position = new Vector3 (target.position.x, 
+		                                  transform.position.y, 
+		                                  transform.position.z);
+	}
+}
